4b95f83f41e56b11109d3a2e490fce8746788663
futsuuu <futsuuu123@gmail.com>
2024-02-11 01:52:39 +0900

    fix(scripts): fix DLL name

--- scripts/get_lua_dll.ps1
+++ scripts/get_lua_dll.ps1
@@ -2,17 +2,16 @@ Param(
   [ValidateSet("5.1", "5.2", "5.3", "5.4")]$version
 )
 
-$semver = switch ($version) {
-  "5.1" { "5.1.5" }
-  "5.2" { "5.2.4" }
-  "5.3" { "5.3.6" }
-  "5.4" { "5.4.2" }
+$semver, $dll = switch ($version) {
+  "5.1" { "5.1.5", "lua5.1.dll" }
+  "5.2" { "5.2.4", "lua52.dll" }
+  "5.3" { "5.3.6", "lua53.dll" }
+  "5.4" { "5.4.2", "lua54.dll" }
 }
 $uri = "https://downloads.sourceforge.net/project/luabinaries/${semver}/Windows%20Libraries/Dynamic/lua-${semver}_Win64_dll17_lib.zip"
 Invoke-WebRequest -Uri $uri -Method 'GET' -Outfile 'lualib.zip' -UserAgent 'PowerShell'
 Expand-Archive -Force -Path 'lualib.zip' -DestinationPath 'lualib'
 
-$dll = "lua$($version -replace '\.', '').dll"
-Move-Item -Force "lualib/lua${version}.dll" $dll
+Move-Item -Force "lualib/${dll}" "lua$($version -replace '\.', '').dll"
 
 Remove-Item -Recurse -Force 'lualib.zip', 'lualib'
