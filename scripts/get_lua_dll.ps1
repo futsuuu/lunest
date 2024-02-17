ba468babf193ce700d98c5eeaf4ce3ec40edf2ac
futsuuu <105504350+futsuuu@users.noreply.github.com>
2024-02-17 16:15:22 +0900

    ci: add ci.yml for GitHub Actions (#1)
    * feat(script): get DLLs for all versions of Lua at once
    
    * ci: setup GitHub Actions for testing

--- scripts/get_lua_dll.ps1
+++ scripts/get_lua_dll.ps1
@@ -1,17 +1,22 @@
-Param(
-  [ValidateSet("5.1", "5.2", "5.3", "5.4")]$version
-)
+function Get-LuaDLL {
+  Param (
+    [ValidateSet("51", "52", "53", "54")]$version
+  )
+  $semver, $dll = switch ($version) {
+    "51" { "5.1.5", "lua5.1.dll" }
+    "52" { "5.2.4", "lua52.dll" }
+    "53" { "5.3.6", "lua53.dll" }
+    "54" { "5.4.2", "lua54.dll" }
+  }
+  $uri = "https://downloads.sourceforge.net/project/luabinaries/${semver}/Windows%20Libraries/Dynamic/lua-${semver}_Win64_dll17_lib.zip"
+  Invoke-WebRequest -Uri $uri -Method 'GET' -Outfile 'lualib.zip' -UserAgent 'PowerShell'
+  Expand-Archive -Force -Path 'lualib.zip' -DestinationPath 'lualib'
 
-$semver, $dll = switch ($version) {
-  "5.1" { "5.1.5", "lua5.1.dll" }
-  "5.2" { "5.2.4", "lua52.dll" }
-  "5.3" { "5.3.6", "lua53.dll" }
-  "5.4" { "5.4.2", "lua54.dll" }
+  Move-Item -Force "lualib/${dll}" "lua${version}.dll"
+  Remove-Item -Recurse -Force 'lualib.zip', 'lualib'
 }
-$uri = "https://downloads.sourceforge.net/project/luabinaries/${semver}/Windows%20Libraries/Dynamic/lua-${semver}_Win64_dll17_lib.zip"
-Invoke-WebRequest -Uri $uri -Method 'GET' -Outfile 'lualib.zip' -UserAgent 'PowerShell'
-Expand-Archive -Force -Path 'lualib.zip' -DestinationPath 'lualib'
 
-Move-Item -Force "lualib/${dll}" "lua$($version -replace '\.', '').dll"
-
-Remove-Item -Recurse -Force 'lualib.zip', 'lualib'
+Get-LuaDLL "51"
+Get-LuaDLL "52"
+Get-LuaDLL "53"
+Get-LuaDLL "54"
