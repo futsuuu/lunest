d520aa5891cd4db5eda30b14dc297d34a31bcf2a
futsuuu <futsuuu123@gmail.com>
2024-12-31 08:14:09 +0900

    feat(bundler): support for multiple module roots

--- build.rs
+++ build.rs
@@ -1,14 +1,14 @@
-use std::{env, fs, path::PathBuf};
-
-use anyhow::Result;
-
-fn main() -> Result<()> {
-    let out_dir = PathBuf::from(env::var_os("OUT_DIR").unwrap());
-    let lua_dir = env::current_dir()?.join("lua");
+fn main() -> std::io::Result<()> {
+    let out_dir = std::path::PathBuf::from(std::env::var_os("OUT_DIR").unwrap());
+    let lua_dir = std::env::current_dir()?.join("lua");
     println!("cargo:rerun-if-changed={}", lua_dir.display());
-    fs::write(
+    std::fs::write(
         out_dir.join("main.lua"),
-        bundler::bundle(&lua_dir.join("lunest.lua"))?,
+        bundler::Bundler::new()
+            .modules(lua_dir.join("lunest.lua"))?
+            .public("lunest")
+            .entry_point("lunest")
+            .bundle(),
     )?;
     Ok(())
 }
--- bundler/src/lib.rs
+++ bundler/src/lib.rs
@@ -1,87 +1,100 @@
 use std::{ffi::OsStr, fs, path::Path};
 
-use anyhow::Result;
+#[derive(Default)]
+pub struct Bundler {
+    modules: Vec<Module>,
+    entry_point: Option<String>,
+    publics: Vec<String>,
+}
 
-pub fn bundle(entry_point: &Path) -> Result<String> {
-    let entry_point = fs::canonicalize(entry_point)?;
-    let module_dir = if entry_point.file_name().unwrap() == OsStr::new("init.lua") {
-        entry_point.parent().unwrap().to_path_buf()
-    } else {
-        entry_point.with_extension("")
-    };
+impl Bundler {
+    pub fn new() -> Self {
+        Self::default()
+    }
 
-    let mut modules = Vec::new();
-    modules.push(Module::new(
-        module_dir.parent().unwrap(),
-        &entry_point,
-        true,
-    )?);
-    for entry in walkdir::WalkDir::new(&module_dir) {
-        let entry = entry?;
-        let path = entry.path();
-        if entry.file_type().is_file()
-            && path.extension() == Some(OsStr::new("lua"))
-            && path != entry_point
-        {
-            modules.push(Module::new(
-                module_dir.parent().unwrap(),
-                entry.path(),
-                false,
-            )?);
+    pub fn modules(&mut self, module_root_file: impl AsRef<Path>) -> std::io::Result<&mut Self> {
+        let module_root_file = module_root_file.as_ref();
+        let module_dir = if module_root_file.file_name().unwrap() == OsStr::new("init.lua") {
+            module_root_file.parent().unwrap().to_path_buf()
+        } else {
+            module_root_file.with_extension("")
+        };
+
+        self.modules
+            .push(Module::new(module_dir.parent().unwrap(), module_root_file)?);
+        if module_dir.exists() {
+            for entry in walkdir::WalkDir::new(&module_dir) {
+                let entry = entry?;
+                let path = entry.path();
+                if entry.file_type().is_file()
+                    && path.extension() == Some(OsStr::new("lua"))
+                    && path != module_root_file
+                {
+                    self.modules
+                        .push(Module::new(module_dir.parent().unwrap(), entry.path())?);
+                }
+            }
         }
+
+        Ok(self)
     }
 
-    let mut result = include_str!("./override.lua").replace(
-        "local PUBLIC_MODULES\n",
-        &format!(
-            "local PUBLIC_MODULES = {{ {} }}\n",
-            modules
-                .iter()
-                .filter(|m| m.public)
-                .map(|m| format!("['{}'] = true", m.name))
-                .collect::<Vec<_>>()
-                .join(", ")
-        ),
-    );
-
-    for module in &modules {
-        result += &module.preload();
+    pub fn public(&mut self, module_name: &str) -> &mut Self {
+        self.publics.push(module_name.into());
+        self
     }
 
-    result += &format!(
-        "return require('{}')\n",
-        module_dir.file_name().unwrap().to_str().unwrap()
-    );
+    pub fn entry_point(&mut self, module_name: &str) -> &mut Self {
+        self.entry_point = Some(module_name.into());
+        self
+    }
 
-    Ok(result)
+    pub fn bundle(&self) -> String {
+        let mut result = include_str!("./override.lua").replace(
+            "local PUBLIC_MODULES\n",
+            &format!(
+                "local PUBLIC_MODULES = {{ {} }}\n",
+                self.publics
+                    .iter()
+                    .map(|m| format!("['{m}'] = true"))
+                    .collect::<Vec<_>>()
+                    .join(", ")
+            ),
+        );
+        for module in &self.modules {
+            result += &module.setup_loader();
+        }
+        if let Some(entry_point) = &self.entry_point {
+            result += &format!("return require('{entry_point}')\n");
+        }
+        result
+    }
 }
 
 struct Module {
     name: String,
-    contents: String,
-    public: bool,
+    chunk: String,
 }
 
 impl Module {
-    fn new(root_dir: &Path, path: &Path, public: bool) -> Result<Self> {
-        let contents = fs::read_to_string(path)?.trim().to_string();
-        let path = path.strip_prefix(root_dir).unwrap();
-        let name = if path.file_name().unwrap() == OsStr::new("init.lua") {
-            path.parent().unwrap().display().to_string()
-        } else {
-            path.with_extension("").display().to_string()
-        }
-        .replace(['/', '\\'], ".");
+    fn new(base_dir: &Path, path: &Path) -> std::io::Result<Self> {
         Ok(Self {
-            name,
-            contents,
-            public,
+            name: {
+                let path = path.strip_prefix(base_dir).unwrap();
+                if path.file_name().unwrap() == OsStr::new("init.lua") {
+                    path.parent().unwrap().display().to_string()
+                } else {
+                    path.with_extension("").display().to_string()
+                }
+                .replace(['/', '\\'], ".")
+            },
+            chunk: fs::read_to_string(path)?,
         })
     }
 
-    fn preload(&self) -> String {
+    fn setup_loader(&self) -> String {
         let modname = &self.name;
-        let contents = &self.contents;
-        format!("package.preload['{modname}'] = function(...)\n{contents}\nend\n")
+        let chunk = self.chunk.trim();
+        format!("package.preload['{modname}'] = function(...)\n{chunk}\nend\n")
     }
 }
