18a25d6fdcd2d50aa61cd629647a1c31952ed553
futsuuu <futsuuu123@gmail.com>
2025-01-02 07:58:52 +0900

    refactor: change directory structure

--- cli/build.rs
+++ cli/build.rs
@@ -3,8 +3,8 @@ fn main() -> std::io::Result<()> {
     std::fs::write(
         out_dir.join("main.lua"),
         bundler::Bundler::new()
-            .add_modules("./lua/lunest.lua")?
-            .add_modules("./lib/json.lua/json.lua")?
+            .add_modules("../module/lunest.lua")?
+            .add_modules("../3rd/json.lua/json.lua")?
             .make_public("lunest")
             .bundle(Some("lunest")),
     )
