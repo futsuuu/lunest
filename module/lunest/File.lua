68745471609d61ae2fa4e8699f3e829d58f885c7
futsuuu <futsuuu123@gmail.com>
2025-01-11 06:01:40 +0900

    test

--- module/lunest/File.lua
+++ module/lunest/File.lua
@@ -1,16 +1,29 @@
 ---@class lunest.File
 ---@field [1] file*
 local M = {}
+
+local test = require("lunest.wrapper")
+local assertion = test.assertion
+
 ---@private
 M.__index = M
+
+---@param file file*
+---@return self
+function M.from_raw(file)
+    return setmetatable({ file }, M)
+end
 
 ---@param filename string
 ---@param mode openmode?
 ---@return self
 function M.open(filename, mode)
-    return setmetatable({
-        [1] = assert(io.open(filename, mode)),
-    }, M)
+    return M.from_raw(assert(io.open(filename, mode)))
+end
+
+---@return self
+function M.tmp()
+    return M.from_raw(io.tmpfile())
 end
 
 function M:close()
@@ -21,6 +34,13 @@ end
 function M:is_closed()
     return io.type(self[1]) == "closed file"
 end
+
+test.test("close", function()
+    local file = M.tmp()
+    assert(not file:is_closed())
+    file:close()
+    assert(file:is_closed())
+end)
 
 ---@param ... string | number
 function M:write(...)
@@ -33,12 +53,32 @@ function M:writeln(...)
     self:write("\n")
 end
 
+---@private
+---@param whence seekwhence?
+---@param offset integer?
 ---@return integer
-function M:size()
-    local offset = self[1]:seek("cur")
-    local n = self[1]:seek("end")
-    self[1]:seek("set", offset)
-    return n
+function M:_seek(whence, offset)
+    local offset_, err = self[1]:seek(whence, offset)
+    assert(offset_ and not err, err)
+    return offset_
+end
+
+---@param offset integer?
+---@return integer
+function M:seek(offset)
+    return self:_seek("set", offset)
+end
+
+---@param offset integer?
+---@return integer
+function M:seek_end(offset)
+    return self:_seek("end", -offset)
+end
+
+---@param offset integer?
+---@return integer
+function M:seek_rel(offset)
+    return self:_seek("cur", offset)
 end
 
 if _G["jit"] or _VERSION ~= "Lua 5.1" then
@@ -52,12 +92,20 @@ else
         if not line then
             return
         end
-        self[1]:seek("cur", -1)
+        self:seek_rel(-1)
         if self[1]:read(1) == "\n" then
             line = line .. "\n"
         end
         return line
     end
 end
+
+test.test("readln", function()
+    local file = M.tmp()
+    file:write("hello\nworld")
+    file:seek()
+    assertion.eq("hello\n", file:readln())
+    assertion.eq("world", file:readln())
+end)
 
 return M
